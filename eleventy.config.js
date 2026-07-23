505d3a562cfea9c54443029237af52980df04b46
PoeticRainbow <jarrett.bayer@outlook.com>
2026-07-23 16:51:09 -0400

    migrate to eleventy

---
+++ eleventy.config.js
@@ -0,0 +1,30 @@
+const { DateTime } = require("luxon");
+
+const TIME_ZONE = "America/New_York";
+const TIME_FORMAT = "MMMM d, yyyy 'at' h:mm a ZZZ";
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/blog/*.md").sort((a, b) => b.data.date - a.data.date);
+  });
+
+  eleventyConfig.addFilter("dateFormatted", function (date) {
+    return DateTime.fromJSDate(date).setZone(TIME_ZONE).toFormat("MMMM d, yyyy 'at' h:mm a ZZZZ");
+  });
+
+  // Copy any .jpg file to `_site`, via Glob pattern
+  // Keeps the same directory structure.
+  // eleventyConfig.addPassthroughCopy("**/*.jpg");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "templates",
+    },
+  };
+};
